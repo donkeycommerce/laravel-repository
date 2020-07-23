fcba4589fb28e593e4a9b8b6338977abfa894cc6
danieletulone <danieletulone.work@gmail.com>
2020-07-23 12:58:21 +0200

    Move logic of resource into Repo manager instead of repo class

--- src/Repository.php
+++ src/Repository.php
@@ -2,38 +2,17 @@
 
 namespace DonkeyCommerce\Repository;
 
-use Illuminate\Database\Eloquent\Collection;
-
 /**
  * The base class representing a repository.
  */
 class Repository
 {
     /**
-     * A resource.
-     */
-    private $resource;
-
-    /**
-     * A resources collection.
-     *
-     * @var Collection
-     */
-    private $resources;
-
-    /**
      * The model in use.
      *
      * @var
      */
     protected $model;
-
-    /**
-     * Indicates if operation works with one or multiple resource.
-     *
-     * @var boolean
-     */
-    private $multiple = false;
 
     /**
      * Resolve ims id on initialization.
@@ -45,63 +24,13 @@ class Repository
         $this->model = $model;
     }
 
-    /**
-     * Check if operation works with one or multiple resource.
+     /**
+     * Get current model.
      *
-     * @return  boolean
+     * @return void
      */
-    public function isMultiple(): bool
+    public function getModel()
     {
-        return $this->multiple;
-    }
-
-    /**
-     * Get a resource.
-     *
-     * @param int $index
-     *
-     * @return array|mixed
-     */
-    public function getResource($index = 0)
-    {
-        return $this->isMultiple()
-                     ? $this->resources[$index]
-                     : $this->resource;
-    }
-
-    /**
-     * Set a resource.
-     *
-     * @param $resource
-     * @return  self
-     */
-    public function setResource($resource): self
-    {
-        $this->resource = $resource;
-
-        return $this;
-    }
-
-    /**
-     * Get a resources collection.
-     */
-    public function getResources()
-    {
-        return $this->isMultiple()
-                     ? $this->resources
-                     : new Collection([$this->resource]);
-    }
-
-    /**
-     * Set a resources collection.
-     *
-     * @param $resources
-     * @return  self
-     */
-    public function setResources($resources): self
-    {
-        $this->resources = $resources;
-
-        return $this;
-    }
+        return $this->model;
+    }   
 }
--- src/RepositoryManager.php
+++ src/RepositoryManager.php
@@ -9,11 +9,30 @@ use Illuminate\Support\Str;
 class RepositoryManager
 {
     /**
+     * Indicates if operation works with one or multiple resource.
+     *
+     * @var boolean
+     */
+    private $multiple = false;
+    
+    /**
      * The repository to use.
      *
      * @var Repository
      */
     private $repository;
+
+    /**
+     * A resource.
+     */
+    private $resource;
+
+    /**
+     * A resources collection.
+     *
+     * @var Illuminate\Database\Eloquent\Collection
+     */
+    private $resources;
 
     /**
      * Get a repository.
@@ -48,4 +67,65 @@ class RepositoryManager
 
         return $result;
     }
+
+    /**
+     * Get a resource.
+     *
+     * @param int $index
+     *
+     * @return array|mixed
+     */
+    public function getResource($index = 0)
+    {
+        return $this->isMultiple()
+                     ? $this->resources[$index]
+                     : $this->resource;
+    }
+
+    /**
+     * Set a resource.
+     *
+     * @param $resource
+     * @return  self
+     */
+    public function setResource($resource): self
+    {
+        $this->resource = $resource;
+
+        return $this;
+    }
+
+    /**
+     * Get a resources collection.
+     */
+    public function getResources()
+    {
+        return $this->isMultiple()
+                     ? $this->resources
+                     : new Collection([$this->resource]);
+    }
+
+    /**
+     * Set a resources collection.
+     *
+     * @param $resources
+     * @return  self
+     */
+    public function setResources($resources): self
+    {
+        $this->resources = $resources;
+
+        return $this;
+    }
+
+    /**
+     * Check if operation works with one or multiple resource.
+     *
+     * @return  boolean
+     */
+    public function isMultiple(): bool
+    {
+        return $this->multiple;
+    }
+
 }
